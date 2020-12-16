11b495c3492d846f28a71165e07e8463a5061daa
deniskyashif <denis.kyashif@gmail.com>
2020-12-16 18:43:39 +0200

    feat: run multiple instances on subsequent ports

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
+using System;
 using System.IO;
+using System.Net.Sockets;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
@@ -6,11 +8,33 @@ public class Program
 {
 	public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
 
-	static IHostBuilder CreateHostBuilder(string[] args) => 
-		Host.CreateDefaultBuilder(args)
+	static IHostBuilder CreateHostBuilder(string[] args)
+	{
+		var port = 8080;
+
+		while (IsPortInUse(port))
+			port++;
+
+		return Host.CreateDefaultBuilder(args)
 			.ConfigureWebHostDefaults(webBuilder =>
 			{
+				webBuilder.UseUrls($"http://*:{port}");
 				webBuilder.UseContentRoot(Directory.GetCurrentDirectory());
 				webBuilder.UseStartup<Startup>();
 			});
+	}
+
+	static bool IsPortInUse(int port)
+	{
+		try
+		{
+			using var tcpClient = new TcpClient();
+			tcpClient.Connect("127.0.0.1", port);
+			return true;
+		}
+		catch (Exception)
+		{
+			return false;
+		}
+	}
 }
